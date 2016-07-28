e5104fdb84ee7523827c90b61d360b6466cdc9ac
Suneet Nandwani <suneetn@gmail.com>
2016-07-28 08:21:07 -0700

    another message

--- code_girls_site/polls/views.py
+++ code_girls_site/polls/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Awesome job guys! This is the index page of our polls applications')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_questions': latest_questions
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("This is the detail view of the question: %s" % question_id )
